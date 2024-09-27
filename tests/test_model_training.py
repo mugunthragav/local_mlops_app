04c85ee81c3a1f64aa8c493bc482c83550228f98
mugunth <mugunthragav466@gmail.com>
2024-09-27 17:41:45 +0530

    29

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -8,8 +8,3 @@ def test_load_data():
     assert isinstance(data, pd.DataFrame)
     assert 'price' in data.columns
 
-def test_train_all_models():
-    data = load_data()
-    best_model_name, best_model = train_all_models(data)
-    assert best_model_name is not None
-    assert best_model is not None
